cd6a09a0b29ad975b120e88336abc41e33e18627
Isaac Ongera <isaacongera717@gmail.com>
2023-02-21 09:49:46 +0300

    completed assignment

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,26 @@
+
 class Vehicle
 
-end
+    attr_accessor :wheel_size, :wheel_number
+  
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+  
+    def go
+      "vrrrrrrrooom!"
+    end
+  
+    def fill_up_tank
+      "filling up!"
+    end
+  
+  end
+  
+  class Car < Vehicle
+      def go
+          "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+        end
+  
+  end
